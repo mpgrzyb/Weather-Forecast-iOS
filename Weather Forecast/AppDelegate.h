5acba566efeacd0c28c1728793734b3fc8543f0d
mpgrzyb <mpgrzyb@gmail.com>
2013-12-31 15:54:28 +0100

    Pierwszy commit

---
+++ Weather Forecast/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weather Forecast
+//
+//  Created by Mateusz Grzyb on 31.12.2013.
+//  Copyright (c) 2013 Mateusz Grzyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Weather ForecastTests/Weather_ForecastTests.m
@@ -0,0 +1,34 @@
+//
+//  Weather_ForecastTests.m
+//  Weather ForecastTests
+//
+//  Created by Mateusz Grzyb on 31.12.2013.
+//  Copyright (c) 2013 Mateusz Grzyb. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Weather_ForecastTests : XCTestCase
+
+@end
+
+@implementation Weather_ForecastTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
